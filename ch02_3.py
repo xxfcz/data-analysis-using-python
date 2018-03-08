c301a33e62862c7b91b8879d8b91bc60f82217bf
xxf_cz <xxf_cz@163.com>
2018-03-08 09:58:57 +0800

    使用pandas绘图并显示

--- ch02_3.py
+++ ch02_3.py
@@ -1,9 +1,16 @@
-# P.24 用pandas对时区进行计数
+## P.24 用pandas对时区进行计数
 
 import json
 from pandas import DataFrame, Series
+from matplotlib import interactive
+
+interactive(True)
 
 path = './usagov_bitly_data2012-03-16-1331923249.txt'
 records = [json.loads(line) for line in open(path)]
 frame = DataFrame(records)
+clean_tz = frame['tz'].fillna('Missing')
+clean_tz[clean_tz==''] = 'Unknown'
+tz_counts = clean_tz.value_counts()
+plt = tz_counts[:10].plot(kind='barh')
 
